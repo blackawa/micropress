f356afad36a3f898fe83e73d67c0cb64a1a50af7
blackawa <3104kurokawa@gmail.com>
2016-07-30 13:51:10 +0900

    :sparkles: add compojure

--- src/main/clj/micropress/core.clj
+++ src/main/clj/micropress/core.clj
@@ -1,12 +1,32 @@
 (ns micropress.core
-  (:require [ring.adapter.jetty :as server]))
+  (:require [compojure.core :refer [defroutes context GET]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as server]))
 
 (defonce server (atom nil))
 
-(defn handler [req]
+(defn ok [body]
   {:status 200
-   :headers {"Content-Type" "text/plain; charset=utf-8"}
-   :body "Hello, world"})
+   :body body})
+
+(defn html [res]
+  (assoc res :headaers {"Content-type" "text/html; charset=utf-8"}))
+
+(defn home-view [req]
+  "<h1> home </h1>")
+
+(defn entry [req]
+  "<h1> entry </h1>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+(defroutes handler
+  (GET "/" req home)
+  (GET "/entry/:id" req entry)
+  (route/not-found "<h1>404 Not found</h1>"))
 
 (defn start []
   (when-not @server
