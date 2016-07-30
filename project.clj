c5af6da2b63737f815faec9b9ab44c36087fc6d3
blackawa <3104kurokawa@gmail.com>
2016-07-30 13:42:27 +0900

    :seedling: ディレクトリ構造をclj/cljs用に整理

--- project.clj
+++ project.clj
@@ -14,12 +14,15 @@
                  [cljsjs/react-dom "15.2.1-1"]
                  [sablono "0.7.3"]
                  [org.omcljs/om "1.0.0-alpha40"]
-                 [ring "1.5.0"]]
+                 [ring "1.5.0"]
+                 [compojure "1.5.1"]]
 
   :plugins [[lein-figwheel "0.5.4-7"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
-  :source-paths ["src"]
+  :source-paths ["src" "src/main/clj"]
+  :test-paths ["test" "src/test/clj"]
+  :resource-paths ["src/main/resources"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
@@ -30,24 +33,25 @@
 
                 :compiler {:main micropress.core
                            :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/micropress.js"
-                           :output-dir "resources/public/js/compiled/out"
+                           :output-to "src/main/resources/public/js/compiled/micropress.js"
+                           :output-dir "src/main/resources/public/js/compiled/out"
                            :source-map-timestamp true
                            :preloads [devtools.preload]}}
                {:id "min"
                 :source-paths ["src/main/cljs"]
-                :compiler {:output-to "resources/public/js/compiled/micropress.js"
+                :compiler {:output-to "src/main/resources/public/js/compiled/micropress.js"
                            :main micropress.core
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {:css-dirs ["resources/public/css"]}
+  :figwheel {:css-dirs ["src/main/resources/public/css"]}
 
   :profiles {:dev {:dependencies [[binaryage/devtools "0.7.2"]
                                   [figwheel-sidecar "0.5.4-7"]
-                                  [com.cemerick/piggieback "0.2.1"]]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [alembic "0.3.2"]]
 
-                   :source-paths ["src" "dev"]
+                   :source-paths ["src" "src/main/clj" "dev"]
                    :plugins [[cider/cider-nrepl "0.13.0"]]
                    :repl-options {:init (set! *print-length* 50)
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
--- src/main/cljs/micropress/core.cljs
+++ src/main/cljs/micropress/core.cljs
@@ -6,8 +6,6 @@
 
 (println "This text is printed from src/micropress/core.cljs. Go ahead and edit it and see reloading in action.")
 
-;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom {:text "Hello world!"}))
 
 (om/root
@@ -18,8 +16,4 @@
   app-state
   {:target (. js/document (getElementById "app"))})
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn on-js-reload [])
