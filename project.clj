0260610df57e06b91f17a6c0bd590d645c29b166
blackawa <3104kurokawa@gmail.com>
2016-08-29 12:07:42 +0900

    :up: ragtimeとkormaが同居できるようにjava.jdbcのバージョンを修正

--- project.clj
+++ project.clj
@@ -7,19 +7,23 @@
   :min-lein-version "2.6.1"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;; web application
                  [ring "1.5.0"]
                  [compojure "1.5.1"]
+                 ;; databse connection
+                 [org.clojure/java.jdbc "0.6.1"]
                  [mysql/mysql-connector-java "6.0.3"]
-                 [org.clojure/java.jdbc "0.6.2-alpha2"]
+                 [korma "0.4.3"]
+                 [ragtime "0.6.0"]
+                 ;; other libraries
                  [buddy "1.0.0"]]
 
-  :source-paths ["src" "src/main/clj"]
-  :test-paths ["test" "src/test/clj"]
+  :source-paths ["src/main/clj"]
+  :test-paths ["src/test/clj"]
   :resource-paths ["src/main/resources"]
 
   :clean-targets ^{:protect false} ["target"]
 
-  :profiles {:dev {:dependencies [[alembic "0.3.2"]
-                                  [ragtime "0.6.0"]]
-                   :source-paths ["src" "src/main/clj" "dev"]
+  :profiles {:dev {:dependencies [[alembic "0.3.2"]]
+                   :source-paths ["src/main/clj" "dev"]
                    :repl-options {:init (set! *print-length* 50)}}})
