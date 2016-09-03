7474b177b178808f4d1ac3cb1e7c1f1768608895
blackawa <3104kurokawa@gmail.com>
2016-09-03 10:40:01 +0900

    :shower: 現状要らないレスポンス周りの関数を除去

--- src/main/clj/micropress/util/response.clj
+++ src/main/clj/micropress/util/response.clj
@@ -8,13 +8,3 @@
   {:status  400
    :headers {}
    :body    body})
-
-(defn res-api-ok [body]
-  (let [body (if (string? body) body (pr-str body))]
-    (res/response body)))
-
-(defn html [res]
-  (res/content-type res "text/html; charset=utf-8"))
-
-(defn edn [res]
-  (res/content-type res "application/edn; charset=utf-8"))
