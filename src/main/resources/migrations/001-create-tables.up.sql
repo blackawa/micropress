e1ff163697fdb2b877c88959414ad1b90259eb4a
blackawa <3104kurokawa@gmail.com>
2016-09-07 15:06:35 +0900

    :bug: データモデル不足

--- src/main/resources/migrations/001-create-tables.up.sql
+++ src/main/resources/migrations/001-create-tables.up.sql
@@ -35,11 +35,19 @@ expire_time timestamp not null,
 foreign key (users_id) references users(id)
 );
 --;;
-create table invitations (
+create table invitees (
 id int auto_increment not null primary key,
 invitation_token varchar(30) not null,
 email_address varchar(256) not null,
 expire_time timestamp not null
+);
+--;;
+create table invitee_authorities (
+invitees_id int not null,
+authorities_id int not null,
+primary key (invitees_id, authorities_id),
+foreign key (invitees_id) references invitees(id),
+foreign key (authorities_id) references authorities(id)
 );
 --;;
 create table user_histories (
