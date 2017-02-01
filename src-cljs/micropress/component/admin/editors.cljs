b0cd79e4bc3ba10a62cb713ea3f837d8e00eea00
blackawa <3104kurokawa@gmail.com>
2017-02-01 22:49:09 +0900

    :+1: editor list page

--- src-cljs/micropress/component/admin/editors.cljs
+++ src-cljs/micropress/component/admin/editors.cljs
@@ -6,5 +6,26 @@
 
 (defn editors []
   (reagent/create-class
-   {:reagent-render
-    (fn [] [:div "Editors list"])}))
+   {:component-will-mount
+    (fn []
+      (dispatch [:init-except-route])
+      (auth-token/check)
+      (editor/fetch-all))
+    :reagent-render
+    (fn []
+      (let [data (subscribe [:data])]
+        [:div
+         [:a.ui.green.button {:href "/admin/editors/new"} "invite new editor"]
+         (when @data
+           [:table.ui.celled.table
+            [:thead
+             [:tr [:th "id"] [:th "username"] [:th "status"]]]
+            [:tbody
+             (map
+              (fn [e]
+                [:tr
+                 {:key (:id e)}
+                 [:td (:id e)]
+                 [:td (:username e)]
+                 [:td (:editor_status_id e)]])
+              @data)]])]))}))
