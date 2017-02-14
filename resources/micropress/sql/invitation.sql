9d3980ecec1c8612579f3b7ede65c5c388971c10
blackawa <3104kurokawa@gmail.com>
2017-02-14 13:24:02 +0900

    :sparkles: accept invitation endpoint created!

--- resources/micropress/sql/invitation.sql
+++ resources/micropress/sql/invitation.sql
@@ -5,4 +5,7 @@ values
 (:token, :invitation_status_id)
 
 -- name: find-by-token
-select * from invitation where token = :token
+select * from invitation where token = :token and invitation_status_id = 1
+
+-- name: accept!
+update invitation set invitation_status_id = 2 where id = :id
--- src/micropress/resource/public/invitation.clj
+++ src/micropress/resource/public/invitation.clj
@@ -1,14 +1,29 @@
 (ns micropress.resource.public.invitation
-  (:require [liberator.core :refer [resource]]
+  (:require [buddy.hashers :as h]
+            [liberator.core :refer [resource]]
+            [micropress.repository.editor :as editor]
             [micropress.repository.invitation :as invitation]
             [micropress.resource.base :refer [public]]))
 
 (defn- exists? [ctx db]
-  (not (empty? (invitation/find-by-token (first (:micropress.resource.base/params ctx))))))
+  (let [params (:micropress.resource.base/params ctx)
+        invitation (first (invitation/find-by-token {:token (:token params)} {:connection db}))]
+    [(not (nil? invitation)) {::invitation invitation}]))
+
+(defn- post! [ctx db]
+  (let [data (:micropress.resource.base/data ctx)
+        invitation (::invitation ctx)]
+    (invitation/accept! {:id (:id invitation)} {:connection db})
+    (-> data
+        (update :password h/encrypt)
+        (assoc :editor_status_id 1)
+        (editor/create-editor! {:connection db}))))
 
 (defn invitation [db]
   (resource
    (public)
-   :allowed-methods [:get]
+   :allowed-methods [:get :post]
    :available-media-types ["application/edn"]
-   :exists? #(exists? % db)))
+   :exists? #(exists? % db)
+   :can-post-to-missing? (fn [ctx] false)
+   :post! #(post! % db)))
